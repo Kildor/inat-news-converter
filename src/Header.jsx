9a91bc9e06dfb491760981a9bee8e387412ca051
Kostia (Kildor) Romanov <kromanov@gmail.com>
2020-03-11 21:43:36 +0700

    * add link to github, some styles changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Switcher from './Switcher';
+import githubLogo from './GitHub-Mark-Light-32px.png';
 
 export default ({ clickHandler, isHTML })=> {
 	return (<header className="App-header">
+		<a target='_blank' rel='noopener noreferrer' className='github-link' href='https://github.com/Kildor/inat-news-converter'>
+			<img src={githubLogo} alt='Github' /> Github
+			</a>
 	<h1>iNaturalist конвертер</h1>
 		<Switcher clickHandler={clickHandler} isHTML={isHTML} />
 				</header>);
