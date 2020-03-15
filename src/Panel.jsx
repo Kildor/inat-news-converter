248d3ab498eb206af59f75849199c5d53e24f9e1
Kostia (Kildor) Romanov <kromanov@gmail.com>
2020-03-15 16:22:51 +0700

    * hide panels on mobile

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
+const mqlMobile = window.matchMedia('screen and (max-device-width: 900px)');
 export default class extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { collapsed: false };
+		this.state = { collapsed: mqlMobile.matches };
 	}
 	render() {
 
 		return (
 			<div className={'panel ' + this.props.className + ' ' + (this.state.collapsed ? 'collapsed' : '')}>
-				<header><i className='collapse-toggler' onClick={() => { this.setState({ collapsed: !this.state.collapsed }) }}>▼</i> {this.props.title}</header>
+				<header onClick={() => { this.setState({ collapsed: !this.state.collapsed }) }}><i className='collapse-toggler'>▼</i> {this.props.title}</header>
 				<div className='panel-body'>
 					{this.props.children}
 				</div>
