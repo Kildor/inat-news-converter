b6fdfc189c42bc304a04a094e9a034ab86d2dbef
Kostia (Kildor) Romanov <kromanov@gmail.com>
2020-06-24 20:23:09 +0700

    * use hooks in Help

--- src/Help.jsx
+++ src/Help.jsx
@@ -1,44 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Converter from './Converter';
-import help from './help.md';
+import helpFile from './help.md';
 
 const converter = new Converter({useMarkdown: true});
 
 
-export default class extends React.Component {
+export default () => {
 
-	constructor(props) {
-		super(props);
-		this.state = {showPopup:false, help:''}
-	}
+	const [isActive, setActive] = useState(false);
+	const [help, setHelp] = useState("");
 
-	onClickHandler = ()=> {
-		if (this.state.help==='') {
-			console.dir(help)
-			fetch(help)
-				.then((res)=>res.text())
-				.then((text)=>{
-					this.setState({ help: converter.convert(text), showPopup: !this.state.showPopup});
-					});
-		} else {
-			this.setState({showPopup: !this.state.showPopup});
+
+	useEffect(() => {
+		if (help === '') {
+			fetch(helpFile)
+				.then((res) => res.text())
+				.then((text) => {
+					setHelp(converter.convert(text));
+				});
 		}
-	}
-	render() {
+	});
+
+	const onClickHandler = () => setActive(!isActive);
+
 		return (
 			<>
-				<button onClick={this.onClickHandler} className={'btn-help' + (this.state.showPopup ? ' active' : '')}>?</button>
+				<button onClick={onClickHandler} className={'btn-help' + (isActive ? ' active' : '')}>?</button>
 			<div className='popup popup-help'>
 				<header>
-					<button onClick={this.onClickHandler} className='btn-close'>+</button>
+					<button onClick={onClickHandler} className='btn-close'>+</button>
 					Помощь
 
 				</header>
-					<div className='popup-body' dangerouslySetInnerHTML={{__html: this.state.help}}/>
+					<div className='popup-body' dangerouslySetInnerHTML={{__html: help}}/>
 			</div>
-				<div className='popup-bg' onClick={this.onClickHandler}></div>
+				<div className='popup-bg' onClick={onClickHandler}></div>
 			</>
 		);
-	}
 
 }
